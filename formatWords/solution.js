ad408d4e5f6649070d1eb15e9d85cdec27f9f94e
Josiah Green <JosiahGreen90@gmail.com>
2018-02-22 15:50:29 -0800

    fixed

--- formatWords/solution.js
+++ formatWords/solution.js
@@ -1,11 +1,8 @@
 function formatWords(words){
-  if (words === null) 
+  if (words === null)
     return '';
   let newString = words.filter(Boolean);
-  console.log(newString);
   let string = newString.join(', ');
-  console.log(string);
-  string.replace(/,(?=[^,]+$)/, ' and');
-  console.log(string);
-  return string;
+  let boom = string.replace(/,(?=[^,]+$)/, ' and');
+  return boom;
 }
