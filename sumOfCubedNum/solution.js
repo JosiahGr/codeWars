d8a10f07046190d831c254d03cfee553650d343b
Josiah Green <JosiahGreen90@gmail.com>
2018-02-28 21:31:35 -0800

    solved

--- sumOfCubedNum/solution.js
+++ sumOfCubedNum/solution.js
@@ -1,18 +1,12 @@
 function cubeOdd(arr) {
-  let newArray = arr.slice();
-  let total = 0;
-  for(var i in newArray.length) {
-    let a = newArray[i];
-
-    if (typeof a !== 'number') {
-      return total = undefined;
+  let result = 0;
+  for(let i in arr) {
+    if(isNaN(arr[i])) {
+      return undefined;
+    }
+    if (arr[i] % 2 !== 0) {
+      result += Math.pow(arr[i], 3);
     }
   }
-  newArray.map(function(arr) {
-    if(arr % 2 !== 0) {
-      arr = Math.pow(arr, 3);
-      return total += arr;
-    }
-  });
-  return total;
+  return result;
 }
